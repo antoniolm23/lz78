ae3aabe430cc9a0142b649d2d28997bae68b9ed0
Antonio <antoniolm23@gmail.com>
2013-12-12 19:08:44 +0100

    now it works

--- comp.c
+++ comp.c
@@ -16,8 +16,8 @@ bool extract_extension(char* filename, char* ext) {
 }
 
 /* compressor algorithm for lz78:
- * filename is the path of the file to compress, size is the size of the dictionary
- * compressed name is the compressed file name, it can be chose by the user, 
+ * from is the path of the file to compress, size is the size of the dictionary
+ * to is the compressed file name, it can be chose by the user, 
  * 		otherwise a standard name is put in its place*/
 void compress(char* from, char* to, int size){
 	
@@ -33,8 +33,9 @@ void compress(char* from, char* to, int size){
     size_tmp=size;
     struct bitio* bit;
     header hdr;		//header of the file
-    int result=0, found=0;
-	FILE* file_read;
+    int result=0;
+	int longest_match=1, tmp_longest_match=0;
+	FILE* file_read, *file_write;
     
     //compute how many bit to write in the compressed file 
     while(size_tmp>>=1) blen++;
@@ -78,36 +79,47 @@ void compress(char* from, char* to, int size){
 			
 		//read one byte from the file
 		result=fread(&tmp, 1, 1, file_read);
-		//fprintf(stderr, "%i",tmp );        
+		//fprintf(stderr, "%i ",tmp );        
 		
 		itmp=father;	
-		
+		tmp_longest_match++;
 		//search if we already have the actual string in the dictionary
-		found=dict_search(&father, tmp, dict);
-		//fprintf(stderr, "flag_ret: %i\n", flag);
+		position=dict_search(&father, tmp, dict);
+		//fprintf(stderr, "new_father: %i %i\n", father, position);
 		
-		if(found!=0) {
+		if(position!=0) {
+			if(longest_match < tmp_longest_match) 
+				longest_match = tmp_longest_match;
+			tmp_longest_match=0;
 			//add the string in the dictionary
 			dict_add_word(position, father, tmp, dict);
-			//fprintf(stderr, "%i: %i %i\t", position, father, tmp);
+			//fprintf(stderr, "%i: %i %i\n", position, father, tmp);
 			
 			bitio_write(bit, (uint64_t)itmp, blen);
-			//fprintf(stdout,  "%i\n", itmp);
+			//fprintf(stderr,  "\n");
 			father=tmp;
 		}
-	} while(result!=0 || feof(file_read));
-    
+	} while(result!=0 || !feof(file_read));
     //write the last position reached
     bitio_write(bit, father, blen);
     
     //close all the structures writing EOFC on the compressed file
     bitio_write(bit, EOFC, blen);
-    
+    bitio_close(bit);
+	//fprintf(stderr, "%i\n", longest_match);
+	
+	//write the longest match in the file
+	file_write=fopen(to, "r+b");
+	fseek(file_write, sizeof(int), SEEK_SET); 
+	hdr.longest_match=longest_match;
+	fwrite(&hdr.longest_match, sizeof(int), 1, file_write);
+	fclose(file_write);
+	
     //free all other structures
     suppress_dictionary(dict);
     free(dict);
 	fclose(file_read);
-    bitio_close(bit);
+    
     
     fprintf(stderr, "compression executed\n");
 }
